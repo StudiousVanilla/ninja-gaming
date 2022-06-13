c3c7631000dfa736b82f950fa7a3cecb9aca7200
Oisín Byrne <obyrne@cartrawler.com>
2022-06-13 13:57:36 +0100

    initial commit

---
+++ src/routes/about.svelte
@@ -0,0 +1,19 @@
+<script>
+    
+</script>
+
+<div class="about">
+    <h2>About Us</h2>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas voluptas natus ratione ipsa deserunt nihil aut assumenda. Aut quibusdam maxime ex explicabo totam consequatur et eaque, repellendus id. Consectetur, eveniet.</p>
+    <a href="/guides">View Guides</a>
+    <a href="/">Home</a>
+</div>
+
+
+<style>
+    .about{
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,18 @@
+<script>
+    
+</script>
+
+<div class="index">
+    <h2>Welcome</h2>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum eligendi culpa aspernatur, similique nemo neque quis ipsam rem corporis sed, autem expedita delectus numquam debitis provident. A soluta facere consectetur.</p>
+    <a href="/guides">View Guides</a>
+    <a href="/about">About</a>
+</div>
+
+<style>
+    .index{
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
